86c55f96659d5569453131332a9c71495bcd3884
Sadat Saif <sadat.saif@ucalgary.ca>
2023-12-23 18:09:55 -0700

    Completed the shortest path tutorial

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
-//import '.Node.css';
 
-export default class Node extends Component{
-    constructor(props){
-        super(props);
-        this.state={};
-    }
-    render(){
-        return <div className="node"></div>
-    }
+import "./Node.css";
+
+export default class Node extends Component {
+  render() {
+    const {
+      col,
+      isFinish,
+      isStart,
+      isWall,
+      row,
+      onMouseDown,
+      onMouseEnter,
+      onMouseUp,
+    } = this.props;
+    const extraClassName = isFinish
+      ? "node-finish"
+      : isStart
+      ? "node-start"
+      : isWall
+      ? "node-wall"
+      : "";
+
+      return (
+        <div
+          id={`node-${row}-${col}`}
+          className={`node ${extraClassName}`}
+          onMouseDown={() => onMouseDown(row, col)}
+          onMouseUp={() => onMouseUp()}
+          onMouseEnter={() => onMouseEnter(row, col)}
+        ></div>
+      );
+      
+  }
 }
